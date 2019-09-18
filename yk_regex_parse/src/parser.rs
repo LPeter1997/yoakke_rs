e43016d2c5408f633cd7f560bb1bd9dcab6179fb
LPeter1997 <lenkefi.peti@gmail.com>
2019-09-18 22:47:58 +0200

    Started the parser

--- yk_regex_parse/src/parser.rs
+++ yk_regex_parse/src/parser.rs
@@ -54,63 +54,138 @@ use crate::ast::{Node, Quantifier, GroupingElement};
  *               ;
  *
  * group_atom  ::=
- *               | NON_CLOSE_BRACKET
+ *               | ANY_NONSPECIAL_CHAR
+ *               | '\' ANY_SPECIAL_CHAR
  *               ;
  */
 
-pub fn parse(source: &str) -> Result<Box<Node>, ()> {
-    match parse_alternative(source.chars()) {
-        Ok((n, _)) => n,
-        Err(v) => v,
+/// A small helper to ease the Chars interface a bit
+#[derive(Clone)]
+struct Chars<'a>(std::str::Chars<'a>);
+
+impl <'a> Chars<'a> {
+    fn next(&self) -> Option<(char, Chars<'a>)> {
+        let mut clone = self.clone();
+        match clone.0.next() {
+            Some(c) => Some((c, clone)),
+            None => None,
+        }
     }
 }
 
-type ParseResult<'a, T> = Result<(T, std::str::Chars<'a>), ()>;
+/**
+ * Actual parsing.
+ */
 
-fn parse_alternative(it: std::str::Chars<'_>) -> ParseResult<'_, Box<Node>> {
+pub fn parse(source: &str) -> Result<Box<Node>, ()> {
+    match parse_alternative(Chars(source.chars())) {
+        Ok((n, _)) => Ok(n),
+        Err(v) => Err(v),
+    }
+}
+
+type ParseResult<'a, T> = Result<(T, Chars<'a>), ()>;
+
+fn parse_alternative(it: Chars<'_>) -> ParseResult<'_, Box<Node>> {
+    let (first, it) = parse_sequence(it)?;
+    if let Some(('|', it)) = it.next() {
+        let (second, it) = parse_alternative(it)?;
+        Ok((Box::new(Node::Alternative{ first, second }), it))
+    }
+    else {
+        Ok((first, it))
+    }
+}
+
+fn parse_sequence(it: Chars<'_>) -> ParseResult<'_, Box<Node>> {
+    let (first, it) = parse_quantified(it)?;
+    if let Ok((second, it)) = parse_sequence(it.clone()) {
+        Ok((Box::new(Node::Sequence{ first, second }), it))
+    }
+    else {
+        Ok((first, it))
+    }
+}
+
+fn parse_quantified(it: Chars<'_>) -> ParseResult<'_, Box<Node>> {
+    let (subnode, it) = parse_atom(it)?;
+    if let Ok((quantifier, it)) = parse_quantifier(it.clone()) {
+        Ok((Box::new(Node::Quantified{ subnode, quantifier }), it))
+    }
+    else {
+        Ok((subnode, it))
+    }
+}
+
+// TODO: Implement X{N} (At least N) and {N, M} (Between N and M)
+fn parse_quantifier(it: Chars<'_>) -> ParseResult<'_, Quantifier> {
+    match it.next() {
+        Some(('?', it)) => Ok((Quantifier::Between(0, 1), it)),
+        Some(('+', it)) => Ok((Quantifier::AtLeast(1), it)),
+        Some(('*', it)) => Ok((Quantifier::AtLeast(0), it)),
+
+        _ => Err(()),
+    }
+}
+
+fn parse_atom(it: Chars<'_>) -> ParseResult<'_, Box<Node>> {
+    match it.next() {
+        Some(('(', it)) => {
+            let (node, it) = parse_alternative(it)?;
+            if let Some((')', it)) = it.next() {
+                Ok((node, it))
+            }
+            else {
+                Err(())
+            }
+        },
+
+        Some(('[', it)) => {
+            let (node, it) = parse_grouping(it)?;
+            if let Some((']', it)) = it.next() {
+                Ok((node, it))
+            }
+            else {
+                Err(())
+            }
+        },
+
+        Some(('\\', it)) => {
+            unimplemented!();
+        },
+
+        Some((c, it)) => {
+            unimplemented!();
+        },
+
+        None => Err(()),
+    }
+}
+
+fn parse_grouping(it: Chars<'_>) -> ParseResult<'_, Box<Node>> {
     unimplemented!();
 }
 
-fn parse_sequence(it: std::str::Chars<'_>) -> ParseResult<'_, Box<Node>> {
+fn parse_grouping_element_init(it: Chars<'_>) -> ParseResult<'_, GroupingElement> {
     unimplemented!();
 }
 
-fn parse_quantified(it: std::str::Chars<'_>) -> ParseResult<'_, Box<Node>> {
+fn parse_grouping_element(it: Chars<'_>) -> ParseResult<'_, GroupingElement> {
     unimplemented!();
 }
 
-fn parse_quantifier(it: std::str::Chars<'_>) -> ParseResult<'_, Box<Node>> {
+fn parse_grouping_atom_init(it: Chars<'_>) -> ParseResult<'_, char> {
     unimplemented!();
 }
 
-fn parse_atom(it: std::str::Chars<'_>) -> ParseResult<'_, Box<Node>> {
+fn parse_grouping_atom(it: Chars<'_>) -> ParseResult<'_, char> {
     unimplemented!();
 }
 
-fn parse_grouping(it: std::str::Chars<'_>) -> ParseResult<'_, Box<Node>> {
+fn parse_nonspecial_char(it: Chars<'_>) -> ParseResult<'_, char> {
     unimplemented!();
 }
 
-fn parse_grouping_element_init(it: std::str::Chars<'_>) -> ParseResult<'_, GroupingElement> {
-    unimplemented!();
-}
-
-fn parse_grouping_element(it: std::str::Chars<'_>) -> ParseResult<'_, GroupingElement> {
-    unimplemented!();
-}
-
-fn parse_grouping_atom_init(it: std::str::Chars<'_>) -> ParseResult<'_, char> {
-    unimplemented!();
-}
-
-fn parse_grouping_atom(it: std::str::Chars<'_>) -> ParseResult<'_, char> {
-    unimplemented!();
-}
-
-fn parse_nonspecial_char(it: std::str::Chars<'_>) -> ParseResult<'_, char> {
-    unimplemented!();
-}
-
-fn parse_special_char(it: std::str::Chars<'_>) -> ParseResult<'_, char> {
+fn parse_special_char(it: Chars<'_>) -> ParseResult<'_, char> {
     unimplemented!();
 }
