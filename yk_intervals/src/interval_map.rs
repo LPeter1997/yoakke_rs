f0e8bf2f5ffd1a428b9a00a0759c21cd46140b4f
LPeter1997 <lenkefi.peti@gmail.com>
2019-09-18 19:11:39 +0200

    Fixed severe bug, added test for odd exact cover

--- yk_intervals/src/interval_map.rs
+++ yk_intervals/src/interval_map.rs
@@ -311,8 +311,11 @@ impl <K, V> IntervalMap<K, V> where K : Clone + Ord, V : Clone {
                     lower = self.intervals[idx].0.upper.touching().unwrap();
                     if !between.is_empty() {
                         self.intervals.insert(idx, (between, value.clone()));
+                        idx += 2;
                     }
-                    idx += 2;
+                    else {
+                        idx += 1;
+                    }
                 }
 
                 let between = Interval::with_bounds(lower, last_upper);
@@ -511,5 +514,12 @@ mod interval_map_tests {
         let mut map = ivmap_raw![1..3 => vec![1], 5..7 => vec![1], 9..12 => vec![1], 14..15 => vec![1], 15..17 => vec![1], 18..19 => vec![1]];
         map.insert_and_unify(ri(5..17), vec![2], test_unify);
         assert_eq!(map, ivmap![1..3 => vec![1], 5..7 => vec![1, 2], 7..9 => vec![2], 9..12 => vec![1, 2], 12..14 => vec![2], 14..15 => vec![1, 2], 15..17 => vec![1, 2], 18..19 => vec![1]]);
+    }
+
+    #[test]
+    fn insert_into_map_cover_many_odd_exactly() {
+        let mut map = ivmap_raw![1..3 => vec![1], 5..7 => vec![1], 9..12 => vec![1], 14..15 => vec![1], 15..17 => vec![1], 18..19 => vec![1], 21..24 => vec![1]];
+        map.insert_and_unify(ri(5..19), vec![2], test_unify);
+        assert_eq!(map, ivmap![1..3 => vec![1], 5..7 => vec![1, 2], 7..9 => vec![2], 9..12 => vec![1, 2], 12..14 => vec![2], 14..15 => vec![1, 2], 15..17 => vec![1, 2], 17..18 => vec![2], 18..19 => vec![1, 2], 21..24 => vec![1]]);
     }
 }
