1a508d6ebf1ddcc836dd19cdc3f5e4e1f860b14e
Peter Lenkefi <lenkefi.peti@gmail.com>
2019-10-01 12:11:29 +0200

    Added one more basic lexer test

--- yk_lexer/tests/basic.rs
+++ yk_lexer/tests/basic.rs
@@ -61,3 +61,59 @@ fn lex_spaces() {
         }
     ].iter().cloned(), lexer.iter());
 }
+
+#[test]
+fn lex_simple() {
+    let mut lexer = TokenKind::lexer();
+    lexer.modify(&[], 0..0, "if if_ _if 123(else)");
+    assert_iter_eq([
+        Token{
+            range: 0..2,
+            kind: TokenKind::KwIf,
+            position: Position{ line: 0, column: 0 },
+            lookahead: 1,
+        },
+        Token{
+            range: 3..6,
+            kind: TokenKind::Ident,
+            position: Position{ line: 0, column: 3 },
+            lookahead: 1,
+        },
+        Token{
+            range: 7..10,
+            kind: TokenKind::Ident,
+            position: Position{ line: 0, column: 7 },
+            lookahead: 1,
+        },
+        Token{
+            range: 11..14,
+            kind: TokenKind::IntLit,
+            position: Position{ line: 0, column: 11 },
+            lookahead: 1,
+        },
+        Token{
+            range: 14..15,
+            kind: TokenKind::LP,
+            position: Position{ line: 0, column: 14 },
+            lookahead: 1,
+        },
+        Token{
+            range: 15..19,
+            kind: TokenKind::KwElse,
+            position: Position{ line: 0, column: 15 },
+            lookahead: 1,
+        },
+        Token{
+            range: 19..20,
+            kind: TokenKind::RP,
+            position: Position{ line: 0, column: 19 },
+            lookahead: 0,
+        },
+        Token{
+            range: 20..20,
+            kind: TokenKind::End,
+            position: Position{ line: 0, column: 20 },
+            lookahead: 0,
+        }
+    ].iter().cloned(), lexer.iter());
+}
