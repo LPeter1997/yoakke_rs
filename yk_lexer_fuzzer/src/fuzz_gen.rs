4864b5b02c3fe36c74f13aa8e17a3c00f063160b
Peter Lenkefi <lenkefi.peti@gmail.com>
2019-10-01 14:25:34 +0200

    Made string and fuzz strategies

--- yk_lexer_fuzzer/src/fuzz_gen.rs
+++ yk_lexer_fuzzer/src/fuzz_gen.rs
@@ -19,7 +19,7 @@ pub struct AppendEdit {
 }
 
 impl AppendEdit {
-    pub fn with_gen<G>(g: G) -> Self where G : StringGenStrategy {
+    pub fn with_gen<G>(g: G) -> Self where G : StringGenStrategy + 'static {
         Self{ gen: Box::new(g) }
     }
 }
@@ -27,5 +27,92 @@ impl AppendEdit {
 impl FuzzStrategy for AppendEdit {
     fn make_edit(&self, src: &str) -> (Range<usize>, String) {
         (src.len()..src.len(), self.gen.generate())
+    }
+}
+
+/**
+ * Inserter, that inserts a string to a random position into the document.
+ */
+
+pub struct InsertEdit {
+    gen: Box<dyn StringGenStrategy>,
+}
+
+impl InsertEdit {
+    pub fn with_gen<G>(g: G) -> Self where G : StringGenStrategy + 'static {
+        Self{ gen: Box::new(g) }
+    }
+}
+
+impl FuzzStrategy for InsertEdit {
+    fn make_edit(&self, src: &str) -> (Range<usize>, String) {
+        let offs = rand_range(&(0..(src.len() + 1)));
+        (offs..offs, self.gen.generate())
+    }
+}
+
+/**
+ * Eraser, that only erases from the document.
+ */
+
+pub struct EraseEdit { }
+
+impl EraseEdit {
+    pub fn new() -> Self {
+        Self{ }
+    }
+}
+
+impl FuzzStrategy for EraseEdit {
+    fn make_edit(&self, src: &str) -> (Range<usize>, String) {
+        let min = rand_range(&(0..(src.len() + 1)));
+        let max = rand_range(&(min..(src.len() + 1)));
+        (min..max, "".into())
+    }
+}
+
+/**
+ * Splicer, that erases and inserts some string.
+ */
+
+pub struct SpliceEdit {
+    gen: Box<dyn StringGenStrategy>,
+}
+
+impl SpliceEdit {
+    pub fn with_gen<G>(g: G) -> Self where G : StringGenStrategy + 'static {
+        Self{ gen: Box::new(g) }
+    }
+}
+
+impl FuzzStrategy for SpliceEdit {
+    fn make_edit(&self, src: &str) -> (Range<usize>, String) {
+        let min = rand_range(&(0..(src.len() + 1)));
+        let max = rand_range(&(min..(src.len() + 1)));
+        (min..max, self.gen.generate())
+    }
+}
+
+/**
+ * Chooses from a collection of strategies and uses that.
+ */
+
+pub struct RandomEdit {
+    strats: Vec<Box<dyn FuzzStrategy>>,
+}
+
+impl RandomEdit {
+    pub fn new() -> Self {
+        Self{ strats: Vec::new() }
+    }
+
+    pub fn add<G>(&mut self, g: G) where G : FuzzStrategy + 'static {
+        self.strats.push(Box::new(g));
+    }
+}
+
+impl FuzzStrategy for RandomEdit {
+    fn make_edit(&self, src: &str) -> (Range<usize>, String) {
+        sample(&self.strats).make_edit(src)
     }
 }
--- yk_lexer_fuzzer/src/str_gen.rs
+++ yk_lexer_fuzzer/src/str_gen.rs
@@ -29,3 +29,51 @@ impl StringGenStrategy for RandomStringGenerator {
         rand_string(&self.len, &self.charset)
     }
 }
+
+/**
+ * Random string from a predefined set of strings.
+ */
+
+pub struct RandomTokenGenerator {
+    strs: Vec<String>,
+}
+
+impl RandomTokenGenerator {
+    pub fn new() -> Self {
+        Self{ strs: Vec::new() }
+    }
+
+    pub fn add(&mut self, s: &str) {
+        self.strs.push(s.into());
+    }
+}
+
+impl StringGenStrategy for RandomTokenGenerator {
+    fn generate(&self) -> String {
+        sample(&self.strs).clone()
+    }
+}
+
+/**
+ * Chooses from a collection of strategies and uses that.
+ */
+
+pub struct RandomStringStrategy {
+    strats: Vec<Box<dyn StringGenStrategy>>,
+}
+
+impl RandomStringStrategy {
+    pub fn new() -> Self {
+        Self{ strats: Vec::new() }
+    }
+
+    pub fn add<G>(&mut self, g: G) where G : StringGenStrategy + 'static {
+        self.strats.push(Box::new(g));
+    }
+}
+
+impl StringGenStrategy for RandomStringStrategy {
+    fn generate(&self) -> String {
+        sample(&self.strats).generate()
+    }
+}
