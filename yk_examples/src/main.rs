9307ea417c10ff972d299b0885718ba729ee42ea
Peter Lenkefi <lenkefi.peti@gmail.com>
2019-10-27 18:29:26 +0100

    Testing library with a tiny scripting lang implementation

--- yk_examples/src/main.rs
+++ yk_examples/src/main.rs
@@ -3,6 +3,7 @@ extern crate yk_lexer;
 extern crate yk_parser;
 
 use std::io::{self, BufRead};
+use std::collections::HashMap;
 use yk_lexer::{Token, TokenType, Lexer};
 use yk_parser::{yk_parser, ParseResult, Match};
 
@@ -12,8 +13,8 @@ pub enum TokTy {
     #[end] End,
     #[regex(r"[ \r\n]")] #[ignore] Whitespace,
 
-    #[c_ident] Ident, // Unused in example, just for demonstration
-    #[regex("[0-9]+")] IntLit,
+    #[c_ident] Ident,
+    #[regex(r"[0-9]+")] IntLit,
 
     #[token("+")] Add,
     #[token("-")] Sub,
@@ -28,8 +29,25 @@ pub enum TokTy {
     #[token("==")] Eq,
     #[token("!=")] Neq,
 
+    #[token("!")] Not,
+    #[token("and")] And,
+    #[token("or")] Or,
+
     #[token("(")] LeftParen,
     #[token(")")] RightParen,
+
+    #[token("=")] Asgn,
+
+    // TODO: Builds without these
+    // But there's a weird range (larger..=smaller => ...)
+    // in Ident matching status when these are here
+    /*#[token("if")] KwIf,
+    #[token("else")] KwElse,
+    #[token("while")] KwWhile,
+    #[token("print")] KwPrint,*/
+
+    #[token("{")] LeftBrace,
+    #[token("}")] RightBrace,
 }
 
 mod peg {
@@ -38,12 +56,23 @@ mod peg {
     use yk_lexer::Token;
 
     fn btoi(b: bool) -> i32 { if b { 1 } else { 0 } }
+    fn itob(i: i32) -> bool { i != 0 }
 
     yk_parser!{
         item: Token<TokTy>;
         type: i32;
 
-        expr ::= eq_expr;
+        expr ::= or_expr;
+
+        or_expr ::=
+            | or_expr "or" and_expr { btoi(itob(e0) || itob(e2)) }
+            | and_expr
+            ;
+
+        and_expr ::=
+            | and_expr "and" eq_expr { btoi(itob(e0) && itob(e2)) }
+            | eq_expr
+            ;
 
         eq_expr ::=
             | eq_expr "==" rel_expr { btoi(e0 == e2) }
@@ -66,8 +95,14 @@ mod peg {
             ;
 
         mul_expr ::=
-            | mul_expr "*" atom { e0 * e2 }
-            | mul_expr "/" atom { e0 / e2 }
+            | mul_expr "*" unary_expr { e0 * e2 }
+            | mul_expr "/" unary_expr { e0 / e2 }
+            | unary_expr
+            ;
+
+        unary_expr ::=
+            | "!" unary_expr { btoi(!itob(e1)) }
+            | "-" unary_expr { -e1 }
             | atom
             ;
 
@@ -101,6 +136,75 @@ mod peg {
         fn show_found(t: &Token<TokTy>) -> String {
             t.value.clone()
         }
+    }
+}
+
+pub enum Stmt {
+    If(Box<Expr>, Box<Stmt>, Option<Box<Stmt>>),
+    While(Box<Expr>, Box<Stmt>),
+    Asgn(String, Box<Expr>),
+    Compound(Vec<Box<Stmt>>),
+    Expr(Box<Expr>),
+    Print(Box<Expr>),
+}
+
+pub enum Expr {
+    Ident(String),
+    IntLit(i32),
+    Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
+    Mul(Box<Expr>, Box<Expr>),
+    Div(Box<Expr>, Box<Expr>),
+    Eq(Box<Expr>, Box<Expr>),
+    Neq(Box<Expr>, Box<Expr>),
+    Gr(Box<Expr>, Box<Expr>),
+    Le(Box<Expr>, Box<Expr>),
+    GrEq(Box<Expr>, Box<Expr>),
+    LeEq(Box<Expr>, Box<Expr>),
+    And(Box<Expr>, Box<Expr>),
+    Or(Box<Expr>, Box<Expr>),
+    Neg(Box<Expr>),
+    Not(Box<Expr>),
+}
+
+struct Interpreter {
+    variables: Vec<HashMap<String, i32>>,
+}
+
+impl Interpreter {
+    fn new() -> Self {
+        Self{
+            variables: Vec::new(),
+        }
+    }
+
+    fn execute(&mut self, stmt: Box<Stmt>) {
+        unimplemented!();
+    }
+
+    fn evaluate(&mut self, expr: Box<Expr>) -> i32 {
+        unimplemented!();
+    }
+
+    fn ref_var(&mut self, name: &String) -> i32 {
+        for sc in self.variables.iter().rev() {
+            if let Some(v) = sc.get(name) {
+                return *v;
+            }
+        }
+        panic!("Undefined variable {}!", name);
+    }
+
+    fn set_var(&mut self, name: &String, val: i32) {
+        self.variables.last_mut().unwrap().insert(name.clone(), val);
+    }
+
+    fn push_scope(&mut self) {
+        self.variables.push(HashMap::new());
+    }
+
+    fn pop_scope(&mut self) {
+        self.variables.pop();
     }
 }
 
